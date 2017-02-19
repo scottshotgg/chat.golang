94e5e1d74afbb503fd25020ec1f1b108bbb3c870
scottshotgg <scgaydos@gmail.com>
2017-02-19 00:42:20 -0600

    changed the opencl detection file

--- src/chat.golang/server/ocl_detect.cpp
+++ src/chat.golang/server/ocl_detect.cpp
@@ -10,10 +10,32 @@
 #define MEM_SIZE (128)
 #define MAX_SOURCE_SIZE (0x100000)
  
+
+void getCLDeviceInfo(cl_device_id device_id) {
+	char buffer[10240];
+	cl_uint buf_uint;
+	cl_ulong buf_ulong;
+	printf("  -- %d --\n", device_id);
+	clGetDeviceInfo(device_id, CL_DEVICE_NAME, sizeof(buffer), buffer, NULL);
+	printf("  DEVICE_NAME = %s\n", buffer);
+	clGetDeviceInfo(device_id, CL_DEVICE_VENDOR, sizeof(buffer), buffer, NULL);
+	printf("  DEVICE_VENDOR = %s\n", buffer);
+	clGetDeviceInfo(device_id, CL_DEVICE_VERSION, sizeof(buffer), buffer, NULL);
+	printf("  DEVICE_VERSION = %s\n", buffer);
+	clGetDeviceInfo(device_id, CL_DRIVER_VERSION, sizeof(buffer), buffer, NULL);
+	printf("  DRIVER_VERSION = %s\n", buffer);
+	clGetDeviceInfo(device_id, CL_DEVICE_MAX_COMPUTE_UNITS, sizeof(buf_uint), &buf_uint, NULL);
+	printf("  DEVICE_MAX_COMPUTE_UNITS = %u\n", (unsigned int)buf_uint);
+	clGetDeviceInfo(device_id, CL_DEVICE_MAX_CLOCK_FREQUENCY, sizeof(buf_uint), &buf_uint, NULL);
+	printf("  DEVICE_MAX_CLOCK_FREQUENCY = %u\n", (unsigned int)buf_uint);
+	clGetDeviceInfo(device_id, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(buf_ulong), &buf_ulong, NULL);
+	printf("  DEVICE_GLOBAL_MEM_SIZE = %llu\n", (unsigned long long)buf_ulong);
+}
+
+
+
 int main()
 {
-
-	cl_device_id device_id = NULL;
 	cl_context context = NULL;
 	cl_command_queue command_queue = NULL;
 	cl_mem memobj = NULL;
@@ -52,57 +74,76 @@ int main()
     char name[128];
     char vendor[128];
     char version[128];
-
+	char buffer[10240];
 
     for (int i = 0; i < ret_num_platforms; i++) {
         int err = clGetPlatformInfo(platforms[i], CL_PLATFORM_VENDOR, 128, vendor, NULL);
         err |= clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 128, name, NULL);
         err |= clGetPlatformInfo(platforms[i], CL_PLATFORM_VERSION, 128, version, NULL);
-        
 
-        fprintf(stdout, "Platform %d: %s %s %s\n", i, vendor, name, version);
+        fprintf(stdout, "Platform %d: %s %s %s\n", i + 1, vendor, name, version);
+
+        //ret = clGetPlatformIDs(i, &platform_id, &ret_num_platforms);
+		cl_device_id device_id[32];
+
+		//ret = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL,  100, &device_id, &ret_num_devices);
+		//ret = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL,  100, NULL, &ret_num_devices);
+		ret = clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL,  100, device_id, &ret_num_devices);
+
+		printf("Devices: %d\n", ret_num_devices);
+
+		for(int j = 0; j < ret_num_devices; j++) {
+			getCLDeviceInfo(device_id[j]);
+		}
+
+		
+		printf("\n\n\n");
+		//cl_device_info cldeviceinfo;
+		//clGetDeviceInfo(device_id, cldeviceinfo, );
+
+			
     }
 
-	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
 
-	printf("Devices: %d\n", ret_num_devices);
+
+	//printf(buffer);
 
 	//for(int i = 0; i < ret_num_platforms; i++) {
 	//	printf("%d\n", platform_id);
 	//}
 
 	 
-	/* Create OpenCL context */
-	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+	// /* Create OpenCL context */
+	// context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
 	 
-	/* Create Command Queue */
-	command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+	// /* Create Command Queue */
+	// command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
 	 
-	/* Create Memory Buffer */
-	memobj = clCreateBuffer(context, CL_MEM_READ_WRITE,MEM_SIZE * sizeof(char), NULL, &ret);
+	// /* Create Memory Buffer */
+	// memobj = clCreateBuffer(context, CL_MEM_READ_WRITE,MEM_SIZE * sizeof(char), NULL, &ret);
 	 
-	/* Create Kernel Program from the source */
-	program = clCreateProgramWithSource(context, 1, (const char **)&source_str,
-	(const size_t *)&source_size, &ret);
+	// /* Create Kernel Program from the source */
+	// program = clCreateProgramWithSource(context, 1, (const char **)&source_str,
+	// (const size_t *)&source_size, &ret);
 	 
-	/* Build Kernel Program */
-	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+	// /* Build Kernel Program */
+	// ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
 	 
-	/* Create OpenCL Kernel */
-	kernel = clCreateKernel(program, "hello", &ret);
+	// /* Create OpenCL Kernel */
+	// kernel = clCreateKernel(program, "hello", &ret);
 	 
-	/* Set OpenCL Kernel Parameters */
-	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&memobj);
+	// /* Set OpenCL Kernel Parameters */
+	// ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&memobj);
 	 
-	/* Execute OpenCL Kernel */
-	ret = clEnqueueTask(command_queue, kernel, 0, NULL,NULL);
+	// /* Execute OpenCL Kernel */
+	// ret = clEnqueueTask(command_queue, kernel, 0, NULL,NULL);
 	 
-	/* Copy results from the memory buffer */
-	ret = clEnqueueReadBuffer(command_queue, memobj, CL_TRUE, 0,
-	MEM_SIZE * sizeof(char),string, 0, NULL, NULL);
+	// /* Copy results from the memory buffer */
+	// ret = clEnqueueReadBuffer(command_queue, memobj, CL_TRUE, 0,
+	// MEM_SIZE * sizeof(char),string, 0, NULL, NULL);
 	 
-	/* Display Result */
-	puts(string);
+	// /* Display Result */
+	// puts(string);
 	 
 	/* Finalization */
 	ret = clFlush(command_queue);
